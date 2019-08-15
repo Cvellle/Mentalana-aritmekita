2708fc50a2c73da8b7d97320ca19814bd7ebfb41
Cvellle <cuekisa@yahoo.com>
2019-08-15 20:56:45 +0200

    a

---
+++ js/js.js
@@ -0,0 +1,9 @@
+// $(window).scroll(function(){
+//     if ($(this).scrollTop() > 0) {
+//        $('.head').css('height','00px');
+//        $('.head').css('transiotion','0.1s');
+//        $('.head').css('background','transparent !important');
+//     } else {
+//        $('.head').css('height','3vw');
+//     }
+//   });
